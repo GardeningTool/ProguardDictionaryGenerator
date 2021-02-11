3818d46e9400477ec2ff9d09ddf6c5fed761d23a
Gardening_Tool <54986171+GardeningTool@users.noreply.github.com>
2021-02-11 13:10:57 -0600

    Generator now writes to a file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+file = open('dictionary.txt', 'a', encoding="utf-8")
+
 characters = [
     '﷽', '񴀒', 'n', '淼', '򝸣', '䜑', '繓', '딟', 'y', '慑', '', 'Ҽ', '񖧸', '䨿', '姕', 'i'
     'Ǣ', '󆂼', '򛾝', 'Ж', 'ҋ', '𤞞', '#', '@', '𪞺', 'ӳ',  '썐', '𥖁', '灱', '픿', '䧁', '嫰',
@@ -9,9 +11,13 @@ characters = [
     '񩦱', 'o', 'ᆒ', '<', '񑎾', '񈘵', 'ʤ', 'ʑ', '腞', '􈵻', '慴', '񦑙', '9', 'o', 'ա', 'Q', 'j', 'ʠ'
 ]
 
+print('Generating dictionary...')
+
 for i in range(50000):
     name = ""
     for var in range(150):
         name += characters[random.randint(0, len(characters) - 1)]
-    print(f'{name}')
-   
+    file.write(name)
+
+file.close()
+print('Dictionary generated successfully')
